22f0c02e5b60aa99f2239d8505c20da22523f428
Paul Ogolla <paulotieno2@gmail.com>
2023-02-14 20:52:30 +0300

    write api call function and add home widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+final dio = Dio();
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const MaterialApp(
+      title: 'GEOSEARCH',
+      home: Home(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  void getWeather() async {
+    const apiKey = 'b34fddd3dae4a2eb0ad363b62f98ba1e';
+    const cityName = 'London';
+    const url =
+        'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey';
+    final response = await dio.get(url);
+    print(response);
+  }
+
+  void handleChange() async {}
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Location'),
+        leading: const IconButton(
+          icon: Icon(Icons.search),
+          tooltip: 'Navigation menu',
+          onPressed: null,
         ),
       ),
     );
