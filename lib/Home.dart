ac32139aec27cd84836b21a1f8a27565f06b4e0e
Paul Ogolla <paulotieno2@gmail.com>
2023-02-28 22:06:38 +0300

    update

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -21,6 +22,7 @@ class _MyAppState extends State<MyApp> {
   String? location = 'Nairobi';
   String? apiKey = 'b34fddd3dae4a2eb0ad363b62f98ba1e';
   Map weatherData = {};
+  final TextEditingController _textFieldController = TextEditingController();
 
   Future<void> _fetchWeatherData() async {
     var response = await http.get(Uri.parse(
@@ -28,8 +30,10 @@ class _MyAppState extends State<MyApp> {
     if (response.statusCode == 200) {
       setState(() {
         weatherData = json.decode(response.body);
+        print(weatherData);
       });
-      print(weatherData);
+
+      _textFieldController.clear();
     } else {
       print('error');
     }
@@ -39,35 +43,71 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(5),
-              child: TextField(
-                onChanged: (value) {
-                  location = value;
-                },
+        body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Form(
+                      child: Padding(
+                        padding: const EdgeInsets.all(defaultPadding),
+                        child: TextFormField(
+                          onChanged: (value) {
+                            location = value;
+                            // placeAutocomplete(value);
+                          },
+                          controller: _textFieldController,
+                          textInputAction: TextInputAction.search,
+                          decoration: InputDecoration(
+                            hintText: 'Enter Location',
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                              child: SvgPicture.asset(
+                                'assets/icons/location.svg',
+                                // color: secondaryColor40LightTheme,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: _fetchWeatherData,
+                      icon: const Icon(Icons.search)),
+                  // ElevatedButton.icon(
+                  //   onPressed: _fetchWeatherData,
+                  //   icon: Icon.sea,
+                  //   label: const Text('Search'),
+                  // ),
+                ],
               ),
-            ),
-            ElevatedButton(
-              onPressed: _fetchWeatherData,
-              child: const Text('Get Weather'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text('${weatherData['name']}'),
-            const SizedBox(
-              height: 20,
-            ),
-            Text('${weatherData['main']['temp']} °F'),
-            const SizedBox(
-              height: 20,
-            ),
-            Image.network(
-                'https://openweathermap.org/img/w/${weatherData['weather'][0]['icon']}.png'),
-            Text('${weatherData['weather'][0]['description']}'),
-          ],
+
+              // TextField(
+              //   controller: _textFieldController,
+              //   onChanged: (value) {
+              //     location = value;
+              //   },
+              //   // onSubmitted: _fetchWeatherData,
+              // ),
+
+              const SizedBox(
+                height: 20,
+              ),
+              Text('${weatherData['name']}'),
+              const SizedBox(
+                height: 20,
+              ),
+              Text('${weatherData['main']['temp']} °F'),
+              const SizedBox(
+                height: 20,
+              ),
+              Image.network(
+                  'https://openweathermap.org/img/w/${weatherData['weather'][0]['icon']}.png'),
+              Text('${weatherData['weather'][0]['description']}'),
+            ],
+          ),
         ),
       ),
     );
