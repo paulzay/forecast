6876bb002824724b48af20a20d90199abd1df442
Paul Ogolla <paulotieno2@gmail.com>
2023-03-14 11:36:36 +0300

    make concurrent calls for forecast too

--- lib/Home.dart
+++ lib/Home.dart
@@ -22,21 +22,38 @@ class _MyAppState extends State<MyApp> {
   String? location = 'Nairobi';
   String? apiKey = 'b34fddd3dae4a2eb0ad363b62f98ba1e';
   Map weatherData = {};
+  Map forecast = {};
+  bool loading = false;
   final TextEditingController _textFieldController = TextEditingController();
 
-  Future<void> _fetchWeatherData() async {
-    var response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$apiKey'));
-    if (response.statusCode == 200) {
-      setState(() {
-        weatherData = json.decode(response.body);
-        print(weatherData);
-      });
+  Future _fetchWeatherData() async {
+    setState(() {
+      loading = true;
+    });
 
-      _textFieldController.clear();
+    var responses = await Future.wait([
+      http.get(Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$apiKey&units=metric')),
+      http.get(Uri.parse(
+          "https://api.openweathermap.org/data/2.5/forecast?q=$location&appid=$apiKey&units=metric"))
+    ]);
+
+    print(forecast);
+    // var response = await http.get(Uri.parse(
+    //     'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$apiKey&units=metric'));
+    if (responses[0].statusCode == 200 && responses[1].statusCode == 200) {
+      setState(() {
+        weatherData = json.decode(responses[0].body);
+        forecast = json.decode(responses[1].body);
+        loading = false;
+      });
     } else {
-      print('error');
+      throw 'Error';
     }
+    //   _textFieldController.clear();
+    // } else {
+    //   throw 'Error';
+    // }
   }
 
   @override
@@ -44,70 +61,91 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Form(
-                      child: Padding(
-                        padding: const EdgeInsets.all(defaultPadding),
-                        child: TextFormField(
-                          onChanged: (value) {
-                            location = value;
-                            // placeAutocomplete(value);
-                          },
-                          controller: _textFieldController,
-                          textInputAction: TextInputAction.search,
-                          decoration: InputDecoration(
-                            hintText: 'Enter Location',
-                            prefixIcon: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 10),
-                              child: SvgPicture.asset(
-                                'assets/icons/location.svg',
-                                // color: secondaryColor40LightTheme,
+          child: loading
+              ? const Center(child: CircularProgressIndicator())
+              : Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Form(
+                            child: Padding(
+                              padding: const EdgeInsets.all(defaultPadding),
+                              child: TextFormField(
+                                onChanged: (value) {
+                                  location = value;
+                                  // placeAutocomplete(value);
+                                },
+                                controller: _textFieldController,
+                                textInputAction: TextInputAction.search,
+                                decoration: InputDecoration(
+                                  hintText: 'Enter Location',
+                                  prefixIcon: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 10),
+                                    child: SvgPicture.asset(
+                                      'assets/icons/location.svg',
+                                      // color: secondaryColor40LightTheme,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
                         ),
+                        IconButton(
+                            onPressed: _fetchWeatherData,
+                            icon: const Icon(Icons.search)),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      color: Colors.amberAccent,
+                      margin: const EdgeInsets.all(15.0),
+                      child: Column(
+                        children: [
+                          Text('${weatherData['name']}'),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            children: [
+                              Column(
+                                children: [
+                                  Image.network(
+                                      'https://openweathermap.org/img/w/${weatherData['weather'][0]['icon']}.png'),
+                                  Text(
+                                    '${weatherData['main']['temp']}°',
+                                    style: const TextStyle(
+                                      fontSize: 40.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                      '${weatherData['weather'][0]['description']}'),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text('${weatherData['wind']['speed']} kph'),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text('${weatherData["main"]["humidity"]} %'),
+                                ],
+                              )
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  IconButton(
-                      onPressed: _fetchWeatherData,
-                      icon: const Icon(Icons.search)),
-                  // ElevatedButton.icon(
-                  //   onPressed: _fetchWeatherData,
-                  //   icon: Icon.sea,
-                  //   label: const Text('Search'),
-                  // ),
-                ],
-              ),
-
-              // TextField(
-              //   controller: _textFieldController,
-              //   onChanged: (value) {
-              //     location = value;
-              //   },
-              //   // onSubmitted: _fetchWeatherData,
-              // ),
-
-              const SizedBox(
-                height: 20,
-              ),
-              Text('${weatherData['name']}'),
-              const SizedBox(
-                height: 20,
-              ),
-              Text('${weatherData['main']['temp']} °F'),
-              const SizedBox(
-                height: 20,
-              ),
-              Image.network(
-                  'https://openweathermap.org/img/w/${weatherData['weather'][0]['icon']}.png'),
-              Text('${weatherData['weather'][0]['description']}'),
-            ],
-          ),
+                  ],
+                ),
         ),
       ),
     );
